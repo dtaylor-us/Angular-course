51f2ae8f20e043ef3ff1db4e0b5d1d15a25ee08e
Derek <derektaylor.us@gmail.com>
2017-12-02 10:37:38 -0600

    scope lesson

---
+++ app.js
@@ -0,0 +1,27 @@
+// MODULE
+var angularApp = angular.module('angularApp', []);
+
+// CONTROLLERS
+angularApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+
+    $scope.handle = '';
+
+    $scope.lowercasehandle = function () {
+        return $filter('lowercase')($scope.handle);
+    };
+
+    $scope.$watch('handle', function (newValue, oldValue) {
+        console.info('Changed!');
+        console.log('Old:' + oldValue);
+        console.log('New:' + newValue);
+    });
+
+
+    setTimeout(function() {
+        $scope.$apply(function() {
+            $scope.handle = 'newhandle';
+            console.log('scope has changed');
+        });
+    }, 2000);
+
+}]);
