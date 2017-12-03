837fb12c8c4988efed25a69926f3ab1af8fb137d
Derek <derektaylor.us@gmail.com>
2017-12-03 17:08:25 -0600

    Routing templates and controller lesson

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-// MODULE
-var angularApp = angular.module('angularApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-// CONTROLLERS
-angularApp.controller('mainController', ['$scope', function($scope){
-    $scope.name = 'main';
+myApp.config(function ($routeProvider) {
+    
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+
+    .when('/second/:num', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    });
+    
+});
+
+myApp.service('nameService', function() {
+    var self = this;
+    this.name = 'Derek';
+
+    this.namelength = function() {
+        return self.name.length;
+    };
+});
+
+myApp.controller('mainController', ['$scope', '$log', 'nameService', function($scope, $log, nameService) {
+
+    $log.log(nameService.name);
+    $log.log(nameService.namelength());
+ 
 }]);
 
-angularApp.controller('secondController', ['$scope', function($scope){
-    $scope.name = 'second';
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+    
+    $scope.num = $routeParams.num || 1;
+    
 }]);
