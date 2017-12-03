0ec8b85bbe7daf07db51f9bf1bddc8fb6a732f55
Derek <derektaylor.us@gmail.com>
2017-12-02 23:03:36 -0600

    http request lesson

--- app.js
+++ app.js
@@ -2,11 +2,40 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+angularApp.controller('mainController', [
+    '$scope',
+    '$filter',
+    '$http',
+    function ($scope, $filter, $http) {
 
-    $scope.alertClick = function() {
-        alert('Clicked');
-    };
+        $scope.alertClick = function () {
+            alert('Clicked');
+        };
 
-    $scope.name = 'Iron-man';
-}]);
+        // XMLHttpRequest Example     var xmr = new XMLHttpRequest();
+        // xmr.onreadystatechange = function() {         if (xmr.readyState == 4 &&
+        // xmr.status == 200) {             $scope.rules = JSON.parse(xmr.responseText);
+        //         }     };     xmr.open();     xmr.send(); $http service Example
+        $http
+            .get('/api')
+            .success(function (result) {
+                $scope.rules = result;
+            })
+            .error(function (data, status) {
+                console.log(data);
+            });
+
+        $scope.newRule = '';
+        $scope.addRule = function () {
+            $http
+                .post('/api', {newrule: $scope.newRule})
+                .success(function (result) {
+                    $scope.rules = result;
+                    $scope.newRule = '';
+                })
+                .error(function (data, status) {
+                    console.log(data);
+                });
+        };
+    }
+]);
