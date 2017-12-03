026d84b50f9ce893ecd89b4ac6a246dcb3fb9349
Derek <derektaylor.us@gmail.com>
2017-12-02 23:11:42 -0600

    Multiple controllers, multiple views

--- app.js
+++ app.js
@@ -2,40 +2,10 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', [
-    '$scope',
-    '$filter',
-    '$http',
-    function ($scope, $filter, $http) {
+angularApp.controller('mainController', ['$scope', function($scope){
+    $scope.name = 'main';
+}]);
 
-        $scope.alertClick = function () {
-            alert('Clicked');
-        };
-
-        // XMLHttpRequest Example     var xmr = new XMLHttpRequest();
-        // xmr.onreadystatechange = function() {         if (xmr.readyState == 4 &&
-        // xmr.status == 200) {             $scope.rules = JSON.parse(xmr.responseText);
-        //         }     };     xmr.open();     xmr.send(); $http service Example
-        $http
-            .get('/api')
-            .success(function (result) {
-                $scope.rules = result;
-            })
-            .error(function (data, status) {
-                console.log(data);
-            });
-
-        $scope.newRule = '';
-        $scope.addRule = function () {
-            $http
-                .post('/api', {newrule: $scope.newRule})
-                .success(function (result) {
-                    $scope.rules = result;
-                    $scope.newRule = '';
-                })
-                .error(function (data, status) {
-                    console.log(data);
-                });
-        };
-    }
-]);
+angularApp.controller('secondController', ['$scope', function($scope){
+    $scope.name = 'second';
+}]);
