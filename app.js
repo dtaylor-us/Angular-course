1886539965d805ba40d588304ebf1344774ff8d4
Derek <derektaylor.us@gmail.com>
2017-12-03 20:13:51 -0600

    Creating custom directives lesson

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider) {
-    
-    $routeProvider
-    
-    .when('/', {
-        templateUrl: 'pages/main.html',
-        controller: 'mainController'
-    })
-    
-    .when('/second', {
-        templateUrl: 'pages/second.html',
-        controller: 'secondController'
-    })
 
-    .when('/second/:num', {
-        templateUrl: 'pages/second.html',
-        controller: 'secondController'
-    });
-    
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+        .when('/second/:num', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        });
 });
 
-myApp.service('nameService', function() {
+myApp.service('nameService', function () {
     var self = this;
     this.name = 'Derek';
 
-    this.namelength = function() {
+    this.namelength = function () {
         return self.name.length;
     };
 });
 
-myApp.controller('mainController', ['$scope', '$log', 'nameService', function($scope, $log, nameService) {
+myApp.controller('mainController', ['$scope', '$log', 'nameService', function ($scope, $log, nameService) {
 
-    $log.log(nameService.name);
-    $log.log(nameService.namelength());
- 
+
+
 }]);
 
-myApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
-    
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', function ($scope, $log, $routeParams) {
+
     $scope.num = $routeParams.num || 1;
-    
+
 }]);
+
+
+myApp.directive("searchResult", function ($scope) {
+    return {
+        //restrict: 'AECM',
+        templateUrl: './components/searchresult.html',
+        replace: true
+    };
+});
